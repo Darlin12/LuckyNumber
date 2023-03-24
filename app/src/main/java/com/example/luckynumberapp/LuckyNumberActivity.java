619edc41717fb450a1362c94d39ec0f858fb0521
Darlin Manuel Casado Pérez <70083547+Darlin12@users.noreply.github.com>
2023-03-24 18:48:19 -0400

    Proyecto finalizado.

---
+++ app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
@@ -0,0 +1,90 @@
+package com.example.luckynumberapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Console;
+import java.util.Random;
+
+public class LuckyNumberActivity extends AppCompatActivity {
+
+    TextView title, result;
+    Button btn, back;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lucky_number);
+
+        title = findViewById(R.id.textViewTitle);
+        result = findViewById(R.id.textViewResult);
+        btn = findViewById(R.id.button2);
+        back = findViewById(R.id.buttonAtrás);
+
+        //Incializamos el Intent para obtener la data con el nombre del usuario
+        Intent i = getIntent();
+        String name = i.getStringExtra("name");
+
+        Toast.makeText(this,"El nombre es "+name, Toast.LENGTH_LONG).show();
+        int randomGeneratedNumber = randomNumber();
+
+        result.setText(""+randomGeneratedNumber);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Variable creada para parsea el random numero generado a String
+                String num = String.valueOf(randomGeneratedNumber);
+
+                shareData(name, num);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    //Funcion para generar el random numbre
+    public int randomNumber(){
+        Random r = new Random();
+
+        int number = r.nextInt(1000);
+
+        return number;
+    }
+
+    //Funcion con Intent para compartir el resultado
+    public void shareData(String name, String randomNumber){
+
+        //Implicit Intent
+        // sd = share data
+        Intent sd = new Intent(Intent.ACTION_SEND);
+
+        //Estrableciendo el tipo de dato a enviar
+        sd.setType("text/plain");
+
+        //añadiendo la data al intent
+        sd.putExtra(Intent.EXTRA_SUBJECT, name+ "got lucky today!");
+        sd.putExtra(Intent.EXTRA_TEXT, "His random number is "+randomNumber+"!");
+
+        //Corriendo el Intent
+        startActivity(Intent.createChooser(sd, "Share To:"));
+
+
+    }
+}
---
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.luckynumberapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    EditText editext;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Widgets
+        textview = findViewById(R.id.textView);
+        editext = findViewById(R.id.editName);
+        btn = findViewById(R.id.button);
+
+
+        //onClick Función para capturar el nombre que el usuario a ingresado y pasarlo a la
+        //siguiente pantalla
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //explicit Intent para cambiar de pantalla
+                Intent I = new Intent(getApplicationContext(), LuckyNumberActivity.class);
+
+
+                //Pasando la data del nombre a la siguiente pantalla
+                String user_name = editext.getText().toString();
+                I.putExtra("name", user_name);
+
+                //Correr el explicit Intent
+                startActivity(I);
+            }
+        });
+    }
+}
